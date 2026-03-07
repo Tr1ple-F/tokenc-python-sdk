f4ae9ad1ef399a5ab8f88ae4790eaa4d4fec272f
Tr1ple-F <32393151+Tr1ple-F@users.noreply.github.com>
2026-03-07 17:48:13 -0500

    Added protect_json to types

--- tokenc/types.py
+++ tokenc/types.py
@@ -19,6 +19,7 @@ class CompressionSettings:
     aggressiveness: float = 0.5
     max_output_tokens: Optional[int] = None
     min_output_tokens: Optional[int] = None
+    protect_json: bool = True
 
     def __post_init__(self):
         """Validate compression settings."""
